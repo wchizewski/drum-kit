cab88b60bd6850e583dd0c2094f4f0c45d842d33
wchizewski <90284175+wchizewski@users.noreply.github.com>
2022-09-09 13:39:30 -0600

    vjhk

--- main.js
+++ main.js
@@ -9,59 +9,39 @@ function processKeyDown(e) {
 
   if (e.code === 'KeyA') {
     // Play A-Clap
-    let audio = document.getElementById('clap');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('A').classList.add('playing');
+    setDrum('clap', 'A')
   } else if (e.code === 'KeyS') {
     // Play S-hihat
-    let audio = document.getElementById('hihat');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('S').classList.add('playing');
+    setDrum('hihat', 'S')
   } else if (e.code === 'KeyD') {
     // Play D-kick
-    let audio = document.getElementById('kick');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('D').classList.add('playing');
+    setDrum('kick', 'D')
   } else if (e.code === 'KeyF') {
     // Play F-Openhat
-    let audio = document.getElementById('openhat');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('F').classList.add('playing');
+    setDrum('openhat', 'F')
   } else if (e.code === 'KeyG') {
     // Play G-Boom
-    let audio = document.getElementById('boom');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('G').classList.add('playing');
+    setDrum('boom', 'G')
   } else if (e.code === 'KeyH') {
     // Play H-Ride
-    let audio = document.getElementById('ride');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('H').classList.add('playing');
+    setDrum('ride', 'H')
   } else if (e.code === 'KeyJ') {
     // Play J-Snare
-    let audio = document.getElementById('snare');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('J').classList.add('playing');
+    setDrum('snare', 'J')
   } else if (e.code === 'KeyK') {
     // Play K-Tom
-    let audio = document.getElementById('tom');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('K').classList.add('playing');
+    setDrum('tom', 'K')
   } else if (e.code === 'KeyL') {
     // Play L-Tink
-    let audio = document.getElementById('tink');
+    setDrum('tink', 'L')
+  }
+}
+
+function setDrum(sound, key) {
+  let audio = document.getElementById(sound);
     audio.currentTime = 0;
     audio.play();
-    document.getElementById('L').classList.add('playing');
-  }
+    document.getElementById(key).classList.add('playing');
 }
 
 // Add transition end listeners
